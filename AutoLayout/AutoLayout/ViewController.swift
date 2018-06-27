2d96e406f8b0a888f8133b953eb28c536064c28c
Slavuta75 <slavuta75@gmail.com>
2018-06-27 15:21:17 +0300

    3 method anchor

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -29,27 +29,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
        view.addSubview(view1)
         view.addSubview(view2)
+        creatViewRedConstraint()
+        creatViewGreenConstraint()
 //        creatView1Constrant()
 //         creatView2Constrant()
-        let viewVFL = ["viewRed": view1, "viewGreen": view2]
-        let metrics = ["height": 100, "width": view.bounds.size.width / 3, "top": view.bounds.size.height / 3]
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewRed(height)]|",
-                                                           options: [],
-                                                           metrics: metrics,
-                                                           views: viewVFL))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewGreen(height)]|",
-                                                           options: [],
-                                                           metrics: metrics,
-                                                           views: viewVFL))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[viewRed(width)]-(50)-[viewGreen(width)]-|",
-                                                           options: [],
-                                                           metrics: metrics,
-                                                           views: viewVFL))
-        
-        
+//        let viewVFL = ["viewRed": view1, "viewGreen": view2]
+//        let metrics = ["height": 100, "width": view.bounds.size.width / 3, "top": view.bounds.size.height / 3]
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewRed(height)]|",
+//                                                           options: [],
+//                                                           metrics: metrics,
+//                                                           views: viewVFL))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewGreen(height)]|",
+//                                                           options: [],
+//                                                           metrics: metrics,
+//                                                           views: viewVFL))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[viewRed(width)]-(50)-[viewGreen(width)]-|",
+//                                                           options: [],
+//                                                           metrics: metrics,
+//                                                           views: viewVFL))
+//
+//
     }
 
+    func creatViewRedConstraint() {
+        view1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        view1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        view1.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        view1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
     
+    func creatViewGreenConstraint() {
+        view2.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        view2.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        view2.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        view2.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 //    func creatView1Constrant() {
 //        NSLayoutConstraint(item: view1,
 //                           attribute: .centerX,
